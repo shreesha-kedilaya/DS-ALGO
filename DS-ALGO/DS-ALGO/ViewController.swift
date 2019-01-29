46f5ead94695fc839169a461eeaac9556ecdf78d
Shreesha Kedlaya <shreeshak@desktop-53uguks.cowrks.com>
2019-01-29 12:38:46 +0530

    Project created

---
+++ DS-ALGO/DS-ALGO/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DS-ALGO
+//
+//  Created by Shreesha Kedlaya on 29/01/19.
+//  Copyright © 2019 Shreesha Kedlaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ DS-ALGO/DS-ALGOTests/DS_ALGOTests.swift
@@ -0,0 +1,34 @@
+//
+//  DS_ALGOTests.swift
+//  DS-ALGOTests
+//
+//  Created by Shreesha Kedlaya on 29/01/19.
+//  Copyright © 2019 Shreesha Kedlaya. All rights reserved.
+//
+
+import XCTest
+@testable import DS_ALGO
+
+class DS_ALGOTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
