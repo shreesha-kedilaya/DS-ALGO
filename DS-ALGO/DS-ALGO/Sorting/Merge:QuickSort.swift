1c423b31f9247e0e8364f20be92ad6a118b84a75
Shreesha Kedlaya <Shreesha.Kedlaya@viacom18.com>
2019-02-06 15:33:39 +0530

    Added the hoares algorithm

--- DS-ALGO/DS-ALGO/Sorting/Merge:QuickSort.swift
+++ DS-ALGO/DS-ALGO/Sorting/Merge:QuickSort.swift
@@ -60,21 +60,29 @@ class AdvancedSorting {
     }
     
     // MARK: Quick sort
-    class func quickSort<T: Comparable>(array: [T], _ isOrderedBefore: (T, T) -> Bool) -> [T] {
+    class func quickSortLomuto<T: Comparable>(array: [T], _ isOrderedBefore: (T, T) -> Bool) -> [T] {
         var mutableArray = array
         
-        quickSort(array: &mutableArray, isOrderedBefore, low: 0, high: mutableArray.count - 1)
+        quickSortLomuto(array: &mutableArray, isOrderedBefore, low: 0, high: mutableArray.count - 1)
         
         return mutableArray
     }
     
-    private class func quickSort<T: Comparable>(array: inout [T], _ isOrderedBefore: (T, T) -> Bool, low: Int, high: Int) {
+    class func quickSortHoare<T: Comparable>(array: [T], _ isOrderedBefore: (T, T) -> Bool) -> [T] {
+        var mutableArray = array
+        
+        quickSortHoares(array: &mutableArray, isOrderedBefore, low: 0, high: array.count - 1)
+        
+        return mutableArray
+    }
+    
+    private class func quickSortLomuto<T: Comparable>(array: inout [T], _ isOrderedBefore: (T, T) -> Bool, low: Int, high: Int) {
         guard low < high else { return }
 
         let pivot = lomutoPartition(array: &array, low: low, high: high, isOrderedBefore)
         
-        quickSort(array: &array, isOrderedBefore, low: low, high: pivot - 1)
-        quickSort(array: &array, isOrderedBefore, low: pivot + 1, high: high)
+        quickSortLomuto(array: &array, isOrderedBefore, low: low, high: pivot - 1)
+        quickSortLomuto(array: &array, isOrderedBefore, low: pivot + 1, high: high)
         
     }
 
@@ -92,5 +100,44 @@ class AdvancedSorting {
         array.swapAt(i+1, high)
         
         return i + 1
+    }
+    
+    private class func quickSortHoares<T: Comparable>(array: inout [T], _ isOrderedBefore: (T, T) -> Bool, low: Int, high: Int) {
+        guard low < high else {
+            return
+        }
+        
+        let pivot = partitionHoares(array: &array, low: low, high: high, isOrderedBefore)
+        
+        quickSortHoares(array: &array, isOrderedBefore, low: low, high: pivot)
+        quickSortHoares(array: &array, isOrderedBefore, low: low + 1, high: high)
+    }
+    
+    private class func partitionHoares<T: Comparable>(array: inout [T], low: Int, high: Int, _ isOrderedBefore: (T, T) -> Bool) -> Int {
+        let pivot = array[low]
+        var i = low - 1
+        var j = high + 1
+        
+        print("Entered here \(i) \(j)")
+        while true {
+            repeat {
+                j -= 1
+            } while array[j] > pivot
+            
+            repeat {
+                i += 1
+            } while array[i] < pivot
+            
+            
+            if i < j {
+                array.swapAt(i, j)
+                
+                print("printing i and j \(i) and \(j)")
+                print(array)
+            } else {
+                print("-------------------Returning j \(j)--------------")
+                return j
+            }
+        }
     }
 }
--- DS-ALGO/DS-ALGO/TestingViewController.swift
+++ DS-ALGO/DS-ALGO/TestingViewController.swift
@@ -10,7 +10,9 @@ import UIKit
 
 class TestingViewController: UIViewController {
 
-    var array = [2,3,5,6,1,2,12,14,7,9,55,22]
+//    var array = [2,3,5,6,1,2,12,14,7,9,55,22]
+    var array = [14,1,12,3,7,10,55]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -22,13 +24,16 @@ class TestingViewController: UIViewController {
     
     private func initialize() {
         print("Selection Sort")
-        print(BasicSorting.selectionSort(array: array, <))
-        print("Insertion Sort")
-        print(BasicSorting.insertionSort(array: array, <))
-        print("Buuble Sort")
-        print(BasicSorting.bubbleSort(array: array, <))
-        print("Merge Sort")
-        print(AdvancedSorting.mergeSort(array: array, <))
+//        print(BasicSorting.selectionSort(array: array, <))
+//        print("Insertion Sort")
+//        print(BasicSorting.insertionSort(array: array, <))
+//        print("Buuble Sort")
+//        print(BasicSorting.bubbleSort(array: array, <))
+//        print("Merge Sort")
+//        print(AdvancedSorting.mergeSort(array: array, <))
+        print("Hoares quick sorting")
+        print(AdvancedSorting.quickSortHoare(array: array, <))
+        print("\n\n\n")
 //        print(AdvancedSorting.mergeSort(array: array, <))
     }
 }
